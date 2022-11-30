1fc9a848948119aac67e3d5ffb95df388064c78d
Maryam Hasnaa' Syamila <hasyamil4@gmail.com>
2022-11-30 18:25:28 +0700

    praktikum ddp

---
+++ pertemuan1/for.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i, '-- Hello --')
+print("Hi")
+
+for j  in range(10, 15):
+    print(j, '-- Hello --')
+print("Hello")
+
+for k  in range(10, 15, 2): # nilai pertama: start, nilai kedua: end-1, nilai ketiga: slang/skip 
+    print(k, '-- Hello --')
+print("Annyeong")
+
+total = 0
+for l in range(1, 6):
+    total = total + l
+print(total)
---
+++ pertemuan1/variable.py
@@ -0,0 +1,16 @@
+# tipe data di python
+# 1. string
+# 2. boolean
+# 3. float
+# 4. integer
+# 5. hexadecimal
+# 6. dll
+
+nama = "Hasna"
+umur = 17
+isStudent = True # boolean
+ip = 3.90 # float
+
+Umur20thnMendatang = umur + 20
+
+print("Nama saya", nama, '. Umur saya 20 tahun mendatang adalah', Umur20thnMendatang)
